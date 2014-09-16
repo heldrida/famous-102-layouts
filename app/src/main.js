b51de226228943e405e8235f6e2d3c379075a693
Helder <heldrida@gmail.com>
2014-09-17 00:04:33 +0100

    copied and moved the completion of step2 to it's own file. created 'grid layout step'

--- app/src/main.js
+++ app/src/main.js
@@ -4,43 +4,30 @@ define(function(require, exports, module) {
 
     var Engine = require('famous/core/Engine');
     var Surface = require('famous/core/Surface');
-    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+    var GridLayout = require('famous/views/GridLayout');
 
     var mainContext = Engine.createContext();
 
-    var layout = new HeaderFooterLayout({
-        headerSize: 100,
-        footerSize: 50
+    var grid = new GridLayout({
+        dimensions: [4, 2]
     });
 
-    layout.header.add(new Surface({
-        content: "Header",
-        classes: ["red-bg"],
-        properties: {
-            lineHeight: "100px",
-            textAlign: "center"
-        }
-    }));
+    var surfaces = [];
+    grid.sequenceFrom(surfaces);
 
-    layout.content.add(new Surface({
-        content: "Content",
-        classes: ["grey-bg"],
-        properties: {
-            lineHeight: "400px",
-            textAlign: "center",
-            backgroundColor: "#333"
-        }
-    }));
+    for (var i = 0; i < 8; i++) {
+        surfaces.push(new Surface({
+            content: "panel " + (i + 1),
+            size: [undefined, undefined],
+            properties: {
+                backgroundColor: "hsl(" + (i * 360 / 8) + ", 100%, 50%)",
+                color: "#404040",
+                lineHeight: "200px",
+                textAlign: "center"
+            }
+        }));
+    }
 
-    layout.footer.add(new Surface({
-        content: "Footer",
-        classes: ["red-bg"],
-        properties: {
-            lineHeight: "50px",
-            textAlign: "center"
-        }
-    }));
-
-    mainContext.add(layout);
+    mainContext.add(grid);
 
 });
